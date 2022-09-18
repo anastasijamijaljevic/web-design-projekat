c1a6743608674ee590740083630de2446aa667b6
Anastasija <anastasijamijaljevic11@gmail.com>
2022-09-18 19:26:10 +0200

    make up page

--- validacija.js
+++ validacija.js
@@ -7,4 +7,18 @@ var rezultat = tekst.match(test);
 if (rezultat != null)
 alert("Hvala na ukazanom poverenju!")
 else alert("Molimo unesite podatke!");
+
+var regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+var name = document.getElementById('name').value;
+if(!regName.test(name)){
+    alert('Molimo unesite vaše puno ime.');
+    document.getElementById('name').focus();
+    return false;
+}else{
+    alert('Hvala vam.');
+    return true;
+
+}
+
+
 }
